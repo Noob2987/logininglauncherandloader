a2f68b5f61e2a5225a4048b8979114af5ab6a235
Noob2987 <vova25101178@gmail.com>
2026-06-25 15:17:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,56 +1,51 @@
 const express = require("express");
-const fs = require("fs-extra");
-const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
-const DB_FILE = "./db.json";
-const KEY_FILE = "./key.json";
+const DB = "./db.json";
 
-function loadDB() {
-    return fs.readJsonSync(DB_FILE);
+function load() {
+    return JSON.parse(fs.readFileSync(DB));
+}
+function save(data) {
+    fs.writeFileSync(DB, JSON.stringify(data, null, 2));
 }
 
-function saveDB(db) {
-    fs.writeJsonSync(DB_FILE, db);
-}
-
-function loadKeys() {
-    return fs.readJsonSync(KEY_FILE).keys;
-}
-
-// REGISTER (auto HWID bind)
+// REGISTER (license + HWID bind + expiry)
 app.post("/register", (req, res) => {
     const { login, password, key, hwid } = req.body;
 
-    let db = loadDB();
-    let keys = loadKeys();
+    let db = load();
 
-    if (!keys.includes(key))
+    let license = db.licenses.find(l => l.key === key);
+
+    if (!license)
         return res.json({ status: "invalid_key" });
 
-    if (db.users.find(u => u.login === login))
-        return res.json({ status: "user_exists" });
+    if (license.used)
+        return res.json({ status: "key_used" });
 
     db.users.push({
         login,
         password,
         hwid,
-        key
+        expire: license.expire
     });
 
-    saveDB(db);
+    license.used = true;
+
+    save(db);
 
     res.json({ status: "ok" });
 });
 
-// LOGIN + HWID CHECK
+// LOGIN + HWID + EXPIRY CHECK
 app.post("/login", (req, res) => {
     const { login, password, hwid } = req.body;
 
-    let db = loadDB();
+    let db = load();
 
     let user = db.users.find(u => u.login === login);
 
@@ -60,19 +55,13 @@ app.post("/login", (req, res) => {
     if (user.password !== password)
         return res.json({ status: "wrong_pass" });
 
-    // HWID bind logic
-    if (!user.hwid) {
-        user.hwid = hwid;
-        saveDB(db);
-        return res.json({ status: "first_bind" });
-    }
-
     if (user.hwid !== hwid)
         return res.json({ status: "hwid_fail" });
+
+    if (Date.now() > user.expire)
+        return res.json({ status: "expired" });
 
     res.json({ status: "ok" });
 });
 
-app.listen(3000, () => {
-    console.log("API running on port 3000");
-});
+app.listen(3000, () => console.log("API running"));
